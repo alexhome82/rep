ae8259453fa738c93071e9566af3df279d0ceec4
alexhome82 <alexhome@mail.ru>
2015-11-08 23:41:32 +0300

    CheangeR-2

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("qqqqqqqq");
+            Console.WriteLine("qqqqqggggggggggggggggggggggggggggggggggggggggggggggggqqq");
             Console.Read();
         }
     }
