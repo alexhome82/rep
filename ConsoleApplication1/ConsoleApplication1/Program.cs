d375fc4ad7a6b8c0fb816b4c42fb6a90406de7f6
alexhome82 <alexhome@mail.ru>
2015-11-08 23:49:31 +0300

    cr6

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.Read();
+            Console.WriteLine("ASDFASDFASDF");
             Console.Read();
         }
     }
